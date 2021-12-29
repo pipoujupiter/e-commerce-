f72d749f6a87367dd13f442ac42c26d3ce2a57f9
Auxane <auxane.valembois@outlook.com>
2021-12-29 15:36:27 +0100

    corr image

--- Vue/vueProduit.php
+++ Vue/vueProduit.php
@@ -5,7 +5,7 @@
 
 <div class="produit">
 
-    <img src='Contenu\images\='<?=$produit['image'] ?>/> 
+    <img src='Contenu\images\<?=$produit['image'] ?>'/> 
     <p><?=$produit['name']?><p>
     <p><?=$produit['description']?><p>
     <p><?=$produit['prix']?><p>
